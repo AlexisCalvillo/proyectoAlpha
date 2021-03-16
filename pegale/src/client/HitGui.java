7e78e1f879b422248982b00cd4a1faa8c3e26a9f
Alexis Calvillo <acalvillo@asgbpm.com>
2021-03-15 22:49:25 -0600

    GUI

---
+++ pegale/src/client/HitGui.java
@@ -0,0 +1,19 @@
+package client;
+
+import javax.swing.*;
+
+public class HitGui {
+    private JPanel panel1;
+    private JRadioButton rb1;
+    private JRadioButton rb2;
+    private JRadioButton rb3;
+    private JRadioButton rb4;
+    private JRadioButton rb5;
+    private JRadioButton rb6;
+    private JRadioButton rb7;
+    private JRadioButton rb8;
+    private JRadioButton rb9;
+    private JRadioButton rb10;
+    private JRadioButton rb11;
+    private JRadioButton rb12;
+}
